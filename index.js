b41af49999e95937a44e2bf3da686847da3cbc2c
naman05002 <f20200103@goa.bits-pilani.ac.in>
2022-06-20 00:26:43 +0530

    changed cors

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const app = express();
 
 //connect to database
 connectDB();
-app.use(cors({
-    origin: '*'
-}));
+
 app.use(express.json({extended: false}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 const PORT = process.env.PORT || 5000;
 
 //define routes
