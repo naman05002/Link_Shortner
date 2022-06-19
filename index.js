4b6ed193f307fd332c09338d8451416b81fd3064
naman05002 <f20200103@goa.bits-pilani.ac.in>
2022-06-19 19:29:55 +0530

    base url changed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 app.use(express.json({extended: false}));
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //define routes
 app.use('/',require('./routes/index'));
